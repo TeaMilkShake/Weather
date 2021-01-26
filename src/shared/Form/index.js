60b0c75203aecf3f77f110021e9730b4636025a5
Nikita Yanovskyy <nik.yanovskyy@gmail.com>
2021-01-26 19:59:29 +0200

    checkpoint

--- src/shared/Form/index.js
+++ src/shared/Form/index.js
@@ -3,37 +3,28 @@ import {getCitySuggestions} from '../../api/index'
 import Suggestions from './Suggestions'
 
 const Form = (props) => {
-    const inputRef = useRef()
-    const timeout = (e) => {
-        return setTimeout(async()=>{
-                console.log(e.target.value)
-                let response = await getCitySuggestions(e.target.value)
-                setCities(response.data)
-                console.log(cities)
-            },2000)
-    }
-        
-    const [cities, setCities] = useState([{city: 's'}])
-    const onKeyUp = () =>{
-        clearInterval(timeout)
-    }
-    const onKeyDown = (e) =>{
-        timeout(e)
-    }
-   
-    useEffect(()=>{ 
-        inputRef.current.addEventListener('keyup',onKeyUp)
-        inputRef.current.addEventListener('keydown',onKeyDown)
+    const inputRef = useRef() 
+    const [cities, setCities] = useState([])
+    const [inputValue, setInputValue] = useState("")
+    const id = useRef()
 
-        return ()=>{
-            inputRef.current.removeEventListener('keyup',onKeyUp)
-            inputRef.current.removeEventListener('keydown',onKeyDown)
-            }
-    })
+    const onChange = (e) =>{
+        setInputValue(e.target.value)
+    }
+    const onKeyDown = () =>{
+        clearTimeout(id.current)
+    }
+    const onKeyUp = () =>{
+        return setTimeout(async()=>{
+            let response = await getCitySuggestions(inputValue)
+            setCities(response.data)
+            console.log(cities)
+        },1000)
+    }
     return(
         <form className={"form "+props.locationClass}>
             <div className="text_input_block">
-                <input ref={inputRef} type="text" placeholder="City name"/> 
+                <input ref={inputRef} type="text" placeholder="City name" onKeyUp={onKeyUp} onKeyDown={onKeyDown} onChange={onChange} value={inputValue && inputValue}/> 
                 <Suggestions suggestions={cities}/>
             </div>
             <input type="submit" value="Find"/>
