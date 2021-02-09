b438561b558ef7d0002201f3654e255d7bdcc0ee
Nikita Yanovskyy <nik.yanovskyy@gmail.com>
2021-02-09 19:17:06 +0200

    checkpoint1

--- src/Home/Loader.js
+++ src/Home/Loader.js
@@ -1,21 +1,32 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import {FaCloud} from 'react-icons/fa'
+import {useTransition, animated} from 'react-spring'
 
 const Loader = () =>{
-    return(
-        <div className="home_loader_block">
-            <div className="animated_loader">
-                <FaCloud/>
-                <FaCloud/>
-                <div className="loading_block">
-                    <div className="loading_dot"></div>
-                    <div className="loading_dot"></div>
-                    <div className="loading_dot"></div>
-                    <div className="loading_dot"></div>
-                </div>
-            </div>
-        </div>
+    const [show, set] = useState(true)
+    const transitions = useTransition(show, null, {
+        from: { opacity: 1 },
+        leave: { opacity: 0 },
+    })
+
+
+    return transitions.map(({ item, key, props }) =>
+        item && <animated.div  key={key} style={props}>
+                    <div className="home_loader_block">
+                        <div className="animated_loader">
+                            <FaCloud/>
+                            <FaCloud/>
+                            <div className="loading_block">
+                                <div className="loading_dot"></div>
+                                <div className="loading_dot"></div>
+                                <div className="loading_dot"></div>
+                                <div className="loading_dot"></div>
+                            </div>
+                        </div>
+                    </div>
+                </animated.div>
     )
+    
 }
 
 export default Loader
